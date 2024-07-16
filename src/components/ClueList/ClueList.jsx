d84c2ccb275029bf7a0683a4c2762a864f0d22a1
NickVirga <nsvirga@gmail.com>
2024-07-15 20:40:44 -0400

    on screen keyboard added to tablet media, clue list styles changed to stack lists, one clue changed from IDEAL to IDEA

--- src/components/ClueList/ClueList.jsx
+++ src/components/ClueList/ClueList.jsx
@@ -1,73 +1,93 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import "./ClueList.scss";
 import { GameDataContext } from "../../context/GameDataContext";
 
 function ClueList({ cluesData, getCellsInClue }) {
   const { gameData, updateGameData } = useContext(GameDataContext);
+  const clueRefs = useRef([]);
+
+  useEffect(() => {
+    const clueNum = gameData.selected.clueNum;
+
+    if (clueNum !== null && clueRefs.current[clueNum]) {
+      clueRefs.current[clueNum].scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+      });
+    }
+  }, [gameData.selected.clueNum]);
 
   const handleClickClue = (clue) => {
-    
-    const newCellsInClue = getCellsInClue(clue.cluesIndex, clue.index)
+    const newCellsInClue = getCellsInClue(clue.cluesIndex, clue.index);
 
     updateGameData({
-        ...gameData,
-        selected: {
-          ...gameData.selected,
-          cellsIndex:
-          newCellsInClue[0],
-          cellsInClue: newCellsInClue,
-          clueNum: clue.index
-        },
-      });
+      ...gameData,
+      selected: {
+        ...gameData.selected,
+        cellsIndex: newCellsInClue[0],
+        cellsInClue: newCellsInClue,
+        clueNum: clue.index,
+      },
+    });
   };
 
   return (
     <div className="clue-list">
-      <div className="clue-list__across">
+      <div className="clue-list__section">
         <h3>Across</h3>
-        <ul>
-          {cluesData.clues
-            .filter((clue) => clue.cluesIndex === 0)
-            .map((clue) => {
-              return (
-                <li
-                  key={clue.index}
-                  className={`clue-list__item ${
-                    clue.index === gameData.selected.clueNum
-                      ? "clue-list__item--highlighted"
-                      : ""
-                  }`}
-                  onClick={() => {
-                    handleClickClue(clue);
-                  }}
-                ><span className="clue-list__label">{clue.label}</span>
-                <span className="clue-list__text">{clue.clueText}</span></li>
-              );
-            })}
-        </ul>
+        <div className="clue-list__list-container">
+          <ul className="clue-list__list">
+            {cluesData.clues
+              .filter((clue) => clue.cluesIndex === 0)
+              .map((clue) => {
+                return (
+                  <li
+                    key={clue.index}
+                    className={`clue-list__item ${
+                      clue.index === gameData.selected.clueNum
+                        ? "clue-list__item--highlighted"
+                        : ""
+                    }`}
+                    onClick={() => {
+                      handleClickClue(clue);
+                    }}
+                    ref={(ele) => (clueRefs.current[clue.index] = ele)}
+                  >
+                    <span className="clue-list__label">{clue.label}</span>
+                    <span className="clue-list__text">{clue.clueText}</span>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
       </div>
-      <div className="clue-list__down">
+      <div className="clue-list__section">
         <h3>Down</h3>
-        <ul>
-          {cluesData.clues
-            .filter((clue) => clue.cluesIndex === 1)
-            .map((clue) => {
-              return (
-                <li
-                  key={clue.index}
-                  className={`clue-list__item ${
-                    clue.index === gameData.selected.clueNum
-                      ? "clue-list__item--highlighted"
-                      : ""
-                  }`}
-                  onClick={() => {
-                    handleClickClue(clue);
-                  }}
-                ><span className="clue-list__label">{clue.label}</span>
-                <span className="clue-list__text">{clue.clueText}</span></li>
-              );
-            })}
-        </ul>
+        <div className="clue-list__list-container">
+          <ul className="clue-list__list">
+            {cluesData.clues
+              .filter((clue) => clue.cluesIndex === 1)
+              .map((clue) => {
+                return (
+                  <li
+                    key={clue.index}
+                    className={`clue-list__item ${
+                      clue.index === gameData.selected.clueNum
+                        ? "clue-list__item--highlighted"
+                        : ""
+                    }`}
+                    onClick={() => {
+                      handleClickClue(clue);
+                    }}
+                    ref={(ele) => (clueRefs.current[clue.index] = ele)}
+                  >
+                    <span className="clue-list__label">{clue.label}</span>
+                    <span className="clue-list__text">{clue.clueText}</span>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
       </div>
     </div>
   );
